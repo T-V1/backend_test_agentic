76c37a2ddabbb96e224a80c464973f7d5b4d7d51
T-V1 <teja.vajapeyazula@ascendion.com>
2026-01-30 09:16:02 +0530

    Add files via upload

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,97 @@
+﻿using FirstAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        static private List<Book> books = new List<Book>
+            {
+            new Book
+            {
+                Id = 1,
+                Title = "To Kill a Mockingbird",
+                author = "Harper Lee",
+                YearPublished = 1960
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "1984",
+                author = "George Orwell",
+                YearPublished = 1949
+            },
+            new Book
+            {
+                Id = 3,
+                Title = "The Great Gatsby",
+                author = "F. Scott Fitzgerald",
+                YearPublished = 1925
+            },
+            new Book
+            {
+                Id = 4,
+                Title = "Pride and Prejudice",
+                author = "Jane Austen",
+                YearPublished = 1813
+            },
+            new Book
+            {
+                Id = 5,
+                Title = "The Hobbit",
+                author = "J.R.R. Tolkien",
+                YearPublished = 1937
+            }
+        };
+        [HttpGet]
+        public ActionResult<List<Book>> GetBooks()
+        {
+            return Ok(books);
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBookbyId(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound();
+            return Ok(book);
+        }
+        [HttpPost]
+        public ActionResult<Book> AddBook(Book newBook)
+        {
+            if (newBook == null)
+                return BadRequest();
+            books.Add(newBook);
+            return CreatedAtAction(nameof(GetBookbyId), new { id = newBook.Id }, newBook);
+
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdatedBook(int id, Book updatedBook)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound();
+
+            book.Id = updatedBook.Id;
+            book.Title = updatedBook.Title;
+            book.author = updatedBook.author;
+            book.YearPublished = updatedBook.YearPublished;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound();
+            books.Remove(book);
+            return NoContent();
+        }
+        
+
+    }
+}
