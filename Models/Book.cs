1958c85c56d6109f2463e84a4c1b87c0f7b1a0c8
T-V1 <teja.vajapeyazula@ascendion.com>
2026-01-30 09:15:16 +0530

    Add files via upload

---
+++ Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstAPI.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string author { get; set; }
+
+        public int YearPublished { get; set; }
+    }
+}
